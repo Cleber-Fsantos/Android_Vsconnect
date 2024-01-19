3c263d52471d8de09168b0cd2f862db1e081bd22
Cleberton F. Santos <cleber.cfsantos@gmail.com>
2024-01-18 21:52:44 -0300

    Layout Login finalizado

--- app/src/main/java/com/example/vsconnect/LoginActivity.kt
+++ app/src/main/java/com/example/vsconnect/LoginActivity.kt
@@ -2,8 +2,11 @@ package com.example.vsconnect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.example.vsconnect.databinding.ActivityLoginBinding
 class LoginActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
