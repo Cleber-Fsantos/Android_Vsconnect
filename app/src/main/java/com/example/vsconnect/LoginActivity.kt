38db3c717a9f31ec4ac44c77b6a582facc586969
Cleberton F. Santos <cleber.cfsantos@gmail.com>
2024-01-19 21:54:17 -0300

    Fragment Servico

--- app/src/main/java/com/example/vsconnect/LoginActivity.kt
+++ app/src/main/java/com/example/vsconnect/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.vsconnect
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vsconnect.databinding.ActivityLoginBinding
@@ -10,5 +11,18 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //com binding conseguimos receber informações do Layout
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        //Função Ouvinte de Click -> apos clicar tudo sera executado
+        binding.btnEntrar.setOnClickListener {
+            //variavel mainIntent com a intenção de sair da LoginActivity e ir para MainActivity
+            val mainIntent = Intent(this@LoginActivity, MainActivity::class.java)
+
+            startActivity(mainIntent)
+
+            finish()
+        }
+        //setContentView(binding.root)
     }
 }
